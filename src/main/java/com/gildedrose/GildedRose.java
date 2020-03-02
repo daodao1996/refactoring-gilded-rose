62a458508bf0ed4e4d8d7d067f0fc656c0f5cef9
daodao1996 <yunpeng.ding@thoughtworks.com>
2020-03-02 16:57:01 +0800

    extract variable

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 class GildedRose {
   List<Item> items;
+  private static int CHANGE_UNIT = 1;
 
   public GildedRose(List<Item> items) {
     this.items = items;
@@ -15,21 +16,21 @@ class GildedRose {
         switch (item.name) {
           case "Aged Brie":
             if (lessThanMax(item)) {
-              item.quality += 1;
+              item.quality += CHANGE_UNIT;
             }
             if (item.sell_in < 0 && lessThanMax(item)) {
-              item.quality += 1;
+              item.quality += CHANGE_UNIT;
             }
             break;
           case "Backstage passes to a TAFKAL80ETC concert":
             if (lessThanMax(item)){
-              item.quality += 1;
+              item.quality += CHANGE_UNIT;
             }
             if (item.sell_in < 11 && lessThanMax(item)){
-              item.quality += 1;
+              item.quality += CHANGE_UNIT;
             }
             if (item.sell_in < 6 && lessThanMax(item)){
-              item.quality += 1;
+              item.quality += CHANGE_UNIT;
             }
             if (item.sell_in < 0) {
               item.quality = 0;
@@ -37,10 +38,10 @@ class GildedRose {
             break;
           default:
             if (greaterThanMin(item)) {
-              item.quality -= 1;
+              item.quality -= CHANGE_UNIT;
             }
             if (item.sell_in < 0 && greaterThanMin(item)) {
-              item.quality -= 1;
+              item.quality -= CHANGE_UNIT;
             }
         }
       }
