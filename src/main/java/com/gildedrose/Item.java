1c883b6f517cb779b98ede1d8fbddd9010251160
daodao1996 <yunpeng.ding@thoughtworks.com>
2020-03-03 17:12:40 +0800

    extract udpateSellIn method

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -2,87 +2,91 @@ package com.gildedrose;
 
 public class Item {
 
-    public String name;
+  public String name;
 
-    public int sell_in;
+  public int sell_in;
 
-    public int quality;
+  public int quality;
 
-    public Item(String name, int sell_in, int quality) {
-        this.name = name;
-        this.sell_in = sell_in;
-        this.quality = quality;
-    }
+  public Item(String name, int sell_in, int quality) {
+    this.name = name;
+    this.sell_in = sell_in;
+    this.quality = quality;
+  }
 
-    @Override
-    public String toString() {
-        return this.name + ", " + this.sell_in + ", " + this.quality;
-    }
+  @Override
+  public String toString() {
+    return this.name + ", " + this.sell_in + ", " + this.quality;
+  }
 
-    void udpateItemQuality() {
-        updateQuality();
+  void udpateItemQuality() {
+    updateQuality();
 
-        if (!isSulfuras()) {
-          sell_in = sell_in - 1;
-      }
+    updateSellIn();
 
-      if (sell_in < 0) {
-          if (!isAgedBrie()) {
-              if (!isBackstage()) {
-                  if (quality > 0) {
-                      if (!isSulfuras()) {
-                          quality = quality - 1;
-                      }
-                  }
-              } else {
-                  quality = 0;
-              }
-          } else {
-              if (quality < 50) {
-                  quality = quality + 1;
-              }
+    if (sell_in < 0) {
+      if (!isAgedBrie()) {
+        if (!isBackstage()) {
+          if (quality > 0) {
+            if (!isSulfuras()) {
+              quality = quality - 1;
+            }
           }
+        } else {
+          quality = 0;
+        }
+      } else {
+        if (quality < 50) {
+          quality = quality + 1;
+        }
       }
     }
+  }
 
-    private void updateQuality() {
-        if (!isAgedBrie()
-                && !isBackstage()) {
-            if (quality > 0) {
-                if (!isSulfuras()) {
-                    quality = quality - 1;
-                }
-            }
-        } else {
-            if (quality < 50) {
-                quality = quality + 1;
+  private void updateSellIn() {
+    if (!isSulfuras()) {
+      sell_in = sell_in - 1;
+    }
+  }
 
-                if (isBackstage()) {
-                    if (sell_in < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-
-                    if (sell_in < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-                }
-            }
+  private void updateQuality() {
+    if (!isAgedBrie()
+            && !isBackstage()) {
+      if (quality > 0) {
+        if (!isSulfuras()) {
+          quality = quality - 1;
         }
-    }
+      }
+    } else {
+      if (quality < 50) {
+        quality = quality + 1;
 
-    private boolean isSulfuras() {
-        return name.equals("Sulfuras, Hand of Ragnaros");
-    }
+        if (isBackstage()) {
+          if (sell_in < 11) {
+            if (quality < 50) {
+              quality = quality + 1;
+            }
+          }
 
-    private boolean isBackstage() {
-        return name.equals("Backstage passes to a TAFKAL80ETC concert");
+          if (sell_in < 6) {
+            if (quality < 50) {
+              quality = quality + 1;
+            }
+          }
+        }
+      }
     }
+  }
 
-    private boolean isAgedBrie() {
-        return name.equals("Aged Brie");
-    }
+  private boolean isSulfuras() {
+    return name.equals("Sulfuras, Hand of Ragnaros");
+  }
+
+  private boolean isBackstage() {
+    return name.equals("Backstage passes to a TAFKAL80ETC concert");
+  }
+
+  private boolean isAgedBrie() {
+    return name.equals("Aged Brie");
+  }
 }
