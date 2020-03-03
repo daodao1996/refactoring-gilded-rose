67e7a1bf3bd30ddb13eea362e55174a680207287
daodao1996 <yunpeng.ding@thoughtworks.com>
2020-03-03 15:50:45 +0800

    create text test

--- src/test/java/CanaryTest.java
+++ src/test/java/CanaryTest.java
@@ -1,3 +1,4 @@
+import com.gildedrose.TexttestFixture;
 import com.google.common.io.Files;
 import org.junit.Test;
 
@@ -12,18 +13,10 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class CanaryTest {
-    @Test
-    public void should_be_able_to_run_a_trivial_test_case() {
-        assertThat(1 + 2, is(3));
-    }
 
     @Test
     public void should_be_able_to_use_guava_capabilities() throws IOException {
-        List<Integer> numbers = newArrayList(1, 2, 3, 4, 5);
-        List<Integer> doubled = numbers.stream().map(x -> x * 2).collect(toList());
-        assertThat(doubled, is(newArrayList(2, 4, 6, 8, 10)));
-
-        String src = Files.toString(new File("src/test/java/CanaryTest.java"), UTF_8);
-        assertThat(src.length(), is(954));
+        String src = Files.toString(new File("src/test/java/com/gildedrose/baseline.txt"), UTF_8);
+        assertThat(TexttestFixture.generateText(), is(src));
     }
 }
